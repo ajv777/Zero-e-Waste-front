ca4d70a54385841195a335045f2ff0b9a0a806c8
Ana Junquera Vara <anajunqueravs@gmail.com>
2020-06-03 16:59:20 +0200

    Add product.model.ts

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,4 +1,22 @@
-export class product{
+export class Producto{
+    nombre: string;
+    descripcion: string;
+    categoria: string;
+    fotos: string;
+    precio: number;
+    correo: boolean;
+    mano: boolean;
+    fecha: Date;
 
+    constructor (pNombre: string, pDescripcion: string, pCategoria: string, pFotos: string, pPrecio: number, pCorreo: boolean, pMano: boolean, pFecha: Date) {
+        this.nombre = pNombre;
+        this.descripcion = pDescripcion;
+        this.categoria = pCategoria;
+        this.fotos = pFotos;
+        this.precio = pPrecio;
+        this.correo = pCorreo;
+        this.mano = pMano;
+        this.fecha = pFecha;
+    }
 }
 
